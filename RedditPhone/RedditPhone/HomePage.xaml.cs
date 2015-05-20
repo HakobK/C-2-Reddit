330bf8a79bd95a39c6109b52638208ff051ee040
hakobk <hakobk1@gmail.com>
2015-05-20 14:37:46 +0200

    text

--- RedditPhone/RedditPhone/HomePage.xaml.cs
+++ RedditPhone/RedditPhone/HomePage.xaml.cs
@@ -16,28 +16,63 @@ namespace RedditPhone
     public partial class HomePage : PhoneApplicationPage
     {
 
+
         public HomePage()
         {
             InitializeComponent();
             doStuff();
 
-
         }
 
 
-        static async void doStuff()
+       public async void doStuff()
         {
+           
             Reddit reddit = new Reddit();
-            Subreddit s = new Subreddit();
-            List<Post> swag = new List<Post>();
+
+            await Task.Factory.StartNew(() => {  reddit.LogIn("schoolc2", "school123!"); });
 
             var phone = await Task.Factory.StartNew(() => { return reddit.GetSubreddit("fatpeoplehate"); });
 
-            var posts = phone.Posts.Take(25);
-            foreach(var post in posts)
-            {
-                MessageBox.Show(post.Title);
-            }
+            rName.Text = phone.Title;
+            HotTxt.Text = phone.PublicDescription;
+
+            var posts = await Task.Factory.StartNew(() => { return phone.Posts.Take(5); });
+            MessageBox.Show("1");
+
+            var text = await Task.Factory.StartNew(() => { return posts.Count().ToString(); });
+
+            MessageBox.Show(text);
+
+            string titles = "";
+
+            await Task.Factory.StartNew(() => {
+                foreach (Post post in posts)
+                {
+                    string yolo = post.Title;
+                    TextBlock block = new TextBlock();
+                    block.Text = yolo;
+                    ContentPanel.Children.Add(block);
+
+
+                    
+                }
+            });
+
+
+
+            HotTxt.Text = titles;            
+
+            //foreach(var post in posts)
+            //{
+            //    MessageBox.Show(post.Title);
+            //}
+            //var posts = phone.GetPosts().Take(25);
+
+            //foreach(var post in posts)
+            //{
+            //    MessageBox.Show(post.);
+            //}
 
 
 

0e337b583f0652d5b68baa6f9e4258de2011d7ce
gerbenbergman@hotmail.com <gerbenbergman@hotmail.com>
2015-06-03 15:12:32 -0700

    posts

--- RedditPhone/RedditPhone/PostContent.xaml.cs
+++ RedditPhone/RedditPhone/PostContent.xaml.cs
@@ -7,14 +7,16 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using RedditSharpPCL;
 using System.Threading.Tasks;
 
 namespace RedditPhone
 {
     public partial class PostContent : PhoneApplicationPage
     {
-
-        public string postID;
+        SubredditContent subredditPage = new SubredditContent();
+        public Comment[] comments;
+ 
 
         public PostContent()
         {
@@ -23,26 +25,62 @@ namespace RedditPhone
 
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
-
-            if (NavigationContext.QueryString.ContainsKey("postID"))
+            comments = subredditPage.postListComments;
+            if(comments == null)
             {
-                string postID = NavigationContext.QueryString["postID"];
+                MessageBox.Show("list is empty");
+            }
+            else
+            {
+                MessageBox.Show("List is not empty");
                 await Task.Factory.StartNew(() =>
                 {
-                    Dispatcher.BeginInvoke(() => getPostContent());
-                }
-                );
+                    Dispatcher.BeginInvoke(() =>
+                    { foreach (Comment com in comments) { MessageBox.Show(com.Body); } }
+                    );
+
+                });
             }
+            
+
+            
+
+            //if (NavigationContext.QueryString.ContainsKey("postID"))
+            //{
+            //    postID = NavigationContext.QueryString["postID"];
+            //    await Task.Factory.StartNew(() =>
+            //    {
+            //        Dispatcher.BeginInvoke(() => fillPagewithPost(post));
+            //    }
+            //    );
+            //}
 
         }
 
-        public void getPostContent()
+        //public void fillPagewithPost(Post post)
+        //{
+        //    Post newPost = new Post();
+        //    post.Id = postID;
+        //    //textBoxPost.Text = newPost.Id;
+        //    textBoxPost.Text = post.Title;
+        //    Reddit s = new Reddit();
+        //    Post g = new Post();
+        //    //MessageBox.Show("hallo" + postID);
+        //}
+
+        public void submitComment(Post post, string message)
         {
-            MessageBox.Show(postID);
+
         }
 
+        public void submitCommentComment(Post post, Comment comment, string message)
+        {
 
+        }
+
+        public void voteComment(Post post, Comment comment, string upOrDown)
+        {
+
+        }
     }
-
-    
 }
